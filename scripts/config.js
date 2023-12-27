3cf3ec777692bb0f39fa1532f22466961d206644
Julien <penna.julien@gmail.com>
2023-12-27 18:17:10 +0100

    game splitted in several files

---
+++ scripts/config.js
@@ -0,0 +1,3 @@
+// Déclaration des tableaux contenant les listes des mots et des phrases proposées à l'utilisateur
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
